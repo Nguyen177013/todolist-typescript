4381173183296dcb4133b83d741b576a77399077
Nguyen177013 <phannguyendoremon@gmail.com>
2023-06-16 17:14:06 +0700

    #feature add mark todo and edit todo

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,23 +1,37 @@
 import { useRef, FunctionComponent } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./todo/store";
-import { todoAction } from "./todo/todo-slice";
+import { titleSchema, todoAction, todosSchema } from "./todo/todo-slice";
+import { Checkbox } from "@mui/material";
 const Content: FunctionComponent = () => {
     const dispatch = useDispatch();
-    const {todoInput, todos} = useSelector((state : RootState) =>state.todoList);
-    const inputFocus = useRef<HTMLInputElement>(null);    
+    const { todoInput, todos, pending } = useSelector((state: RootState) => state.todoList);
+    const inputFocus = useRef<HTMLInputElement>(null);
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         let value = e.target.value;
         dispatch(todoAction.setInut(value));
     }
+
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        dispatch(todoAction.setInut(""));
-        dispatch(todoAction.addData(todoInput));
-        inputFocus.current?.focus();
+        if(pending == null){
+            dispatch(todoAction.addData(todoInput));
+            dispatch(todoAction.setInut(""));
+            inputFocus.current?.focus();
+        }
+        else{
+            dispatch(todoAction.editTitle({index:pending, title: todoInput}));
+            dispatch(todoAction.setInut(""));
+        }
     }
     const handleDelete = (index: number) => {
         dispatch(todoAction.deleteData(index));
+    }
+    const handleStatus = (index: number) => {
+        dispatch(todoAction.changeStatus(index));
+    }
+    const setInput = (todo: titleSchema) => {
+        dispatch(todoAction.changeTitle(todo));
     }
     return (
         <div>
@@ -28,7 +42,7 @@ const Content: FunctionComponent = () => {
                     placeholder="type todo stuff"
                     ref={inputFocus}
                 />
-                <button>Save</button>
+                <button>{pending == null ? "Add" : "Change"}</button>
             </form>
             <div>
                 <p>Todo list:</p>
@@ -36,8 +50,24 @@ const Content: FunctionComponent = () => {
                     {todos.map((todo, index) =>
                         <li
                             key={index}
-                        >{todo}
-                            <button onClick={() => handleDelete(index)}>Delete</button>
+                            style={todo.status ? { color: "#ccc" } : {}}
+                        >{todo.title}
+                            {todo.status ||
+                                <button
+                                    onClick={() =>
+                                        setInput({ index: index, title: todo.title })}>
+                                    Edit
+                                </button>
+                            }
+                            <button
+                                onClick={() => handleDelete(index)}
+                            >
+                                Delete
+                            </button>
+                            <Checkbox
+                                checked={todo.status} size="small"
+                                onChange={() => handleStatus(index)}
+                            />
                         </li>
                     )}
                 </ul>
--- src/todo/todo-slice.ts
+++ src/todo/todo-slice.ts
@@ -2,19 +2,66 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     todoInput: "",
-    todos: ['Chơi Genshin', 'Học React Hook', 'Thủ dâm vào 3h sáng']
-};;
+    todos: [
+        {
+            title: 'Chơi Genshin',
+            status: false
+        },
+        {
+            title: 'Học React Hook',
+            status: false
+        },
+        {
+            title: 'Masturbate at 3 A.M',
+            status: true
+        }
+    ],
+    pending: null 
+};
+interface TodoItem {
+    title: string,
+    status: boolean
+}
+interface MyObject {
+    todoInput: string
+    pending: number | null;
+    todos: TodoItem[]
+}
+export interface titleSchema{
+    index:number,
+    title: string
+}
+export type todosSchema = {
+    title: string,
+    status: boolean
+}
 const todoSlice = createSlice({
     name: "todo",
     initialState: initialState,
     reducers: {
-        setInut(state, payload: PayloadAction<string>) {
+        setInut(state: MyObject, payload: PayloadAction<string>) {
             state.todoInput = payload.payload;
         },
-        addData(state, payload: PayloadAction<string>) {
-            state.todos.push(payload.payload);
+        addData(state: MyObject , payload: PayloadAction<string>) {
+            state.todos.push({
+                title: payload.payload,
+                status: false
+            });
         },
-        deleteData(state, payload: PayloadAction<number>) {
+        changeStatus(state: MyObject , payload : PayloadAction<number>){
+            state.todos[payload.payload].status = !state.todos[payload.payload].status;
+        },
+        editTitle(state: MyObject , payload : PayloadAction<titleSchema>){
+            state.todos[payload.payload.index].title = payload.payload.title;
+            state.pending = null;
+        },
+        changeTitle(state: MyObject , payload : PayloadAction<titleSchema>){
+            console.log(payload.payload.index);
+            
+            state.todoInput = payload.payload.title;
+            state.pending = payload.payload.index;
+        },
+        deleteData(state: MyObject , payload: PayloadAction<number>) {
             state.todos.splice(payload.payload, 1);
         }
     }
